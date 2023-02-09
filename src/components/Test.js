8822514516ac89586c422e0243ac4c0c1925bef7
Umar Ahmed <mian.ahmed@ucalgary.ca>
2023-02-08 21:05:42 -0700

    test

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,23 +1,16 @@
 import React from 'react'
 import 'aframe'
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
-import * as THREE from 'three' 
 
 export default function Test() {
-    const loader = new GLTFLoader()
     return (
         <a-scene embedded arjs>
-            <a-marker preset='hiro'>
-                <a-box 
-                    src="https://i.imgur.com/mYmmbrp.jpg" 
-                    position="-1 0.5 -3" 
-                    rotation="0 45 0" 
-                    color="#4CC3D9"
-                    animation="property: object3D.position.y; to: 2.2; dir: alternate; dur: 4000; loop: true"
-                ></a-box>
+            <a-marker preset="hiro">
+                <a-box color="blue" material="opacity: 1;" position="0 0.09 0" scale="0.4 0.8 0.8"></a-box>
             </a-marker>
-            <a-marker-camera preset='hiro'></a-marker-camera>
             <a-entity camera></a-entity>
         </a-scene>
     )
 }
+
+
+
